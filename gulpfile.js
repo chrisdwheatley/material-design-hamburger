349fcabd12d33aeca7f35ba739e2be6a939edc99
Chris Wheatley <chriswheatley87@gmail.com>
2014-10-18 23:15:01 +0100

    gulp build steps & package deps added

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var autoprefixer = require('gulp-autoprefixer');
+var csslint = require('gulp-csslint');
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task('jsbuild', function() {
+  return gulp.src('src/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('cssbuild', function() {
+  return gulp.src('src/*.css')
+    .pipe(csslint())
+    .pipe(csslint.reporter())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/*.css', ['cssbuild']);
+  gulp.watch('src/*.js', ['jsbuild']);
+});
+
+gulp.task('build', ['cssbuild', 'jsbuild']);
