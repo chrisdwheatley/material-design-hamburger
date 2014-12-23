e1cf47dd6338331bf80c0ee237a957354e025978
Chris Wheatley <chriswheatley87@gmail.com>
2014-12-23 05:12:59 +0000

    remove umd code from src, add to build process

--- dist/material-design-hamburger.js
+++ dist/material-design-hamburger.js
@@ -1,25 +1,30 @@
-!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.materialDesignHamburger=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-
-  module.exports = materialDesignHamburger;
-
-  function materialDesignHamburger() {
-    'use strict';
-
-    document.querySelector('.material-design-hamburger__icon').addEventListener(
-        'click',
-        function() {
-      var child = this.childNodes[1].classList;
-
-      if (child.contains('material-design-hamburger__icon--to-arrow')) {
-        child.remove('material-design-hamburger__icon--to-arrow');
-        child.add('material-design-hamburger__icon--from-arrow');
-      } else {
-        child.remove('material-design-hamburger__icon--from-arrow');
-        child.add('material-design-hamburger__icon--to-arrow');
-      }
-
-    });
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.materialDesignHamburger = factory();
   }
+}(this, function() {
+function materialDesignHamburger() {
+  'use strict';
 
-},{}]},{},[1])(1)
-});
+  document.querySelector('.material-design-hamburger__icon').addEventListener(
+      'click',
+      function() {
+    var child = this.childNodes[1].classList;
+
+    if (child.contains('material-design-hamburger__icon--to-arrow')) {
+      child.remove('material-design-hamburger__icon--to-arrow');
+      child.add('material-design-hamburger__icon--from-arrow');
+    } else {
+      child.remove('material-design-hamburger__icon--from-arrow');
+      child.add('material-design-hamburger__icon--to-arrow');
+    }
+
+  });
+}
+
+return materialDesignHamburger;
+}));
