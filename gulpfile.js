dc2b487d77d856e5312c218c9b9606dcbb70b5c8
Chris Wheatley <chriswheatley87@gmail.com>
2014-12-23 05:12:23 +0000

    add umd into build process

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,20 @@ var autoprefixer = require('gulp-autoprefixer');
 var csslint = require('gulp-csslint');
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var umd = require('gulp-umd');
 
 gulp.task('jsbuild', function() {
   return gulp.src('src/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
+    .pipe(umd({
+      exports: function(file) {
+        return 'materialDesignHamburger';
+      },
+      namespace: function(file) {
+        return 'materialDesignHamburger';
+      }
+    }))
     .pipe(gulp.dest('dist'));
 });
 
